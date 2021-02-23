12df542d57cd20dd0ef20e07df4f4e67647dc406
sonamk15 <sonamk18@navgurukul.org>
2021-02-24 00:32:05 +0530

    services done

---
+++ index.js
@@ -0,0 +1,52 @@
+
+const express = require("express")
+const app = express()
+
+const knex = require('./connectWithDB');
+
+app.use(express.json());
+app.post("/add",(req,res,next)=>{
+    const user = {
+        name:req.body.name,
+        image:req.body.image,
+        phone:req.body.phone,
+        gender:req.body.gender,
+        email:req.body.email,
+        address:req.body.address,
+        dob:req.body.dob
+    }
+    knex('users').insert(user).then(() => {
+        console.log("done")
+    })
+    return res.json(user)
+
+})
+
+app.get('/get',(req,res,next)=>{
+    console.log("Sonam")
+    knex.select("*").from('users').then((users)=>{
+       return res.send(users)
+    })
+})
+
+
+app.delete('/delete/:userId',(req,res,next)=>{
+    console.log("sonam")
+    var userId = (req.params.userId)
+    console.log(userId);
+    knex.select("*").from('users').where('id',userId).delete().then(()=>{
+        console.log("user deleted")
+        return res.send("User has been deleted!")
+    })
+})
+
+
+app.delete('/deleteAll',(req,res,next)=>{
+      knex.select("*").from('users').delete().then(()=>{
+        console.log("deleted all users")
+        return res.send("deleted all users!")
+    })
+})
+
+
+app.listen(3000, () => console.log('server is listening'));
